50cbc51647dbe5b0ae18cf04257f0deab062ac1f
Markus Paasovaara <markus.paasovaara@futurice.com>
2017-09-17 19:59:06 +0300

    Delay-DecoratorNodes

--- src/behave/models/DecoratorNode.java
+++ src/behave/models/DecoratorNode.java
@@ -126,4 +126,63 @@ public abstract class DecoratorNode implements Node {
             return Types.Status.Success;
         }
     }
+
+    public abstract static class DelayRunningNode extends DecoratorNode {
+        protected long m_startTime;
+        protected long m_delayMs;
+
+        public DelayRunningNode(long delayInMs) {
+            m_delayMs = delayInMs;
+        }
+
+        @Override
+        public void initialize(ExecutionContext context) {
+            super.initialize(context);
+            m_startTime = System.currentTimeMillis();
+        }
+    }
+
+    public static class DelayBeforeRunningNode extends DelayRunningNode {
+        public DelayBeforeRunningNode(long delayInMs) {
+            super(delayInMs);
+        }
+
+        @Override
+        public Types.Status tick(ExecutionContext context) {
+            long elapsed = System.currentTimeMillis() - m_startTime;
+            if (elapsed < m_delayMs) {
+                return Types.Status.Running;
+            }
+            else {
+                return m_child.tick(context);
+            }
+        }
+    }
+
+
+    public static class DelayAfterRunningNode extends DelayRunningNode {
+        private boolean m_ranOnce = false;
+        private Types.Status m_status = Types.Status.Failure;
+
+        public DelayAfterRunningNode(long delayInMs) {
+            super(delayInMs);
+        }
+
+        @Override
+        public Types.Status tick(ExecutionContext context) {
+            if (!m_ranOnce) {
+                m_status = m_child.tick(context);
+                m_ranOnce = true;
+            }
+
+            long elapsed = System.currentTimeMillis() - m_startTime;
+            if (elapsed < m_delayMs) {
+                return Types.Status.Running;
+            }
+            else {
+                return m_status;
+            }
+        }
+    }
+
 }
