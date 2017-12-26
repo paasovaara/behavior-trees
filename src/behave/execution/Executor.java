c128b2f68ea5be4ac3f100b2c3f89683ca082cf1
Markus Paasovaara <markus.paasovaara@futurice.com>
2017-12-26 16:46:57 +0200

    Replace hardcoded period with properly calculated one in Executor

--- src/behave/execution/Executor.java
+++ src/behave/execution/Executor.java
@@ -12,6 +12,7 @@ public class Executor {
     private Node m_root;
     private ExecutionContext m_context;
     private Timer m_timer;
+    private long m_lastTickTimestamp = 0;
 
     public void initialize(Node root) {
         initialize(root, new ExecutionContext());
@@ -26,21 +27,30 @@ public class Executor {
         //In this early phase we use a simple Timer to execute the traversal.
         //This tick could come from external source also, such as some event/game-loop
         //TODO create separate classes, TimerExecutor and ExternalExecutor
+        m_lastTickTimestamp = System.currentTimeMillis();
+
         m_timer = new Timer();
         m_root.initialize(m_context);
         m_timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                //let's assume timer is deterministic, but TODO measure the step using System.currentTimeInMillis
-                Log.debug("Executor running one Tick");
-                m_context.setLastTimeStep(period);
-                Types.Status status = m_root.tick(m_context);
-                if (status != Types.Status.Running) {
-                    Log.info("Root node has finished with status " + status + ". Ending execution.");
-                    stop();
-                }
+                tickNode();
             }
         }, delay, period);
+    }
+
+    protected void tickNode() {
+        Log.debug("Executor running one Tick");
+        long period = System.currentTimeMillis() - m_lastTickTimestamp;
+
+        m_context.setLastTimeStep(period);
+        Types.Status status = m_root.tick(m_context);
+        if (status != Types.Status.Running) {
+            Log.info("Root node has finished with status " + status + ". Ending execution.");
+            Log.info(m_context.toString());
+            stop();
+        }
+        m_lastTickTimestamp = System.currentTimeMillis();
     }
 
     public void stop() {
